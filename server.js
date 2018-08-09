2dcb51882ac6be9c2f8f7eaa016ee52ff49398fd
Pradeep <here.pradeepa@gmail.com>
2018-08-09 15:55:34 +0530

    Add Post for todos

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
-
 var express = require('express');
- var app =  express();
- var PORT = 3000;
-var middleware ={
-	requireAuthentication: function(req,res,next){
-		console.log('private route');
-		next();
-	},
-	logger: function(req,res,next){
-		
-		console.log('request '+new Date().toString()+ ' ' +req.method+' '+req.originalUrl);
-	     next();
-	}
+var bodyParser = require('body-parser');
+var app = express();
+var PORT =process.env.PORT||3000;
+var todos =[];
+var todoNextId=1;
+app.use(bodyParser.json());
+app.get('/', function (req,res) {
+	res.send('todo API ');
+});
+app.get('/todos',function(req,res) {
+      res.json(todos);
+});
+app.get('/todos/:id',function(req,res){
+	var todoId = parseInt (req.params.id,10);
+	var matchedTodo;
 
-};
-//app.use(middleware.requireAuthentication);
- app.use(middleware.logger);
- app.get('/about',middleware.requireAuthentication,function(req,res){
- 	
- 	res.send('about us?');
- });
-
- app.use(express.static(__dirname + '/public'));
-app.listen(3000,function () {
-	console.log('express server started '+ PORT);
-});	
+	todos.forEach(function(todo){
+		if(todoId===todo.id)
+		{
+			matchedTodo = todo; 
+		}
+	});
+		if(matchedTodo){
+			res.json(matchedTodo);
+		}else{
+			res.status(404).send();
+		}
+	});
+app.post('/todos',function(req,res){
+    	var body = req.body;
+    	body.id = todoNextId++;
+    	todos.push(body);
+    	//console.log('description'+body.description);
+    	res.json(body);			
+});
+app.listen(PORT,function() {
+	console.log('express on port '+PORT);
+});
