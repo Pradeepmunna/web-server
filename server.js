0fd631202ba0f2d76626e88ef62de9e2f68d2c61
Pradeep <here.pradeepa@gmail.com>
2018-08-10 12:09:51 +0530

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _= require('underscore');
 var app = express();
 var PORT =process.env.PORT||3000;
 var todos =[];
@@ -7,33 +8,54 @@ var todoNextId=1;
 app.use(bodyParser.json());
 app.get('/', function (req,res) {
 	res.send('todo API ');
-});
+});		 
 app.get('/todos',function(req,res) {
       res.json(todos);
-});
+});			 
 app.get('/todos/:id',function(req,res){
 	var todoId = parseInt (req.params.id,10);
-	var matchedTodo;
-
-	todos.forEach(function(todo){
-		if(todoId===todo.id)
+	var matchedTodo = _.findWhere(todos,{id:todoId});
+//instead of below if used to match the id we can use the above underscore findwhere to search the object in an array
+	// todos.forEach(function(todo){
+		/*if(todoId===todo.id)
 		{
 			matchedTodo = todo; 
 		}
-	});
-		if(matchedTodo){
+	});*/
+  	if(matchedTodo){
 			res.json(matchedTodo);
 		}else{
 			res.status(404).send();
 		}
 	});
 app.post('/todos',function(req,res){
-    	var body = req.body;
-    	body.id = todoNextId++;
-    	todos.push(body);
-    	//console.log('description'+body.description);
-    	res.json(body);			
+    	var body = _.pick(req.body,'description','completed');		
+if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();		
+}
+				body.description= body.description.trim();
+    	    	body.id = todoNextId++;			
+    	    	todos.push(body);
+    	    	res.json(body);			
 });
 app.listen(PORT,function() {
 	console.log('express on port '+PORT);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
